60149979304c5bc746c23a4c6ca3b8209a4f21c5
Pedro Lopez <plopcas@gmail.com>
2018-10-14 21:26:56 +0100

    CryptoZombies lesson 4 chapter 3

--- cryptozombies/zombiehelper.sol
+++ cryptozombies/zombiehelper.sol
@@ -4,9 +4,26 @@ import "./zombiefeeding.sol";
 
 contract ZombieHelper is ZombieFeeding {
 
+  uint levelUpFee = 0.001 ether;
+
   modifier aboveLevel(uint _level, uint _zombieId) {
     require(zombies[_zombieId].level >= _level);
     _;
+  }
+
+  // 1. Create withdraw function here
+  function withdraw() external onlyOwner {
+    owner.transfer(this.balance);
+  }
+
+  // 2. Create setLevelUpFee function here
+  function setLevelUpFee(uint _fee) external onlyOwner {
+    levelUpFee = _fee;
+  }
+
+  function levelUp(uint _zombieId) external payable {
+    require(msg.value == levelUpFee);
+    zombies[_zombieId].level++;
   }
 
   function changeName(uint _zombieId, string _newName) external aboveLevel(2, _zombieId) {
@@ -21,7 +38,6 @@ contract ZombieHelper is ZombieFeeding {
 
   function getZombiesByOwner(address _owner) external view returns(uint[]) {
     uint[] memory result = new uint[](ownerZombieCount[_owner]);
-    // Start here
     uint counter = 0;
     for (uint i = 0; i < zombies.length; i++) {
       if (zombieToOwner[i] == _owner) {
